bc6af53e30c51c7e731566d1ee2bea7a75f1c4fd
Mateus Alves <mateusalves5942@gmail.com>
2023-03-23 16:43:43 -0300

    ajustando

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const mapa = document.querySelector('svg')
+
+
 const pesquisarEstado = async (estado) => {
     const url = `http://localhost:8080/estado/${estado}`
     const response = await fetch(url)
@@ -13,19 +16,40 @@ const pesquisarEstado = async (estado) => {
     }
 }
 
-const mapa = document.querySelector('svg')
+const pesquisarCidades = async (estado) => {
+    const url = `http://localhost:8080/cidadesEstado/${estado}`
+    const response = await fetch(url)
+    const data = await response.json()
 
-const getEstados = (event) => {
-    const estado = event.target.id.replace('BR-','')
-    preencherDados(estado)
+    return {
+        cidades: data.cidades
+    }
 }
 
 const preencherDados = async (sigla) => {
     document.getElementById('estado').value
     const estado = await pesquisarEstado(sigla)
+    const cidades = await pesquisarCidades(sigla)
 
     document.getElementById('sigla').innerHTML = estado.uf
-    document.getElementById('nome').innerHTML = estado.descricao
+    document.getElementById('nome').innerHTML = estado.nome
+    document.getElementById('capital').innerHTML = estado.capital
+    document.getElementById('regiao').innerHTML = estado.regiao
+
+    const ul = document.getElementById('lista')
+    ul.textContent = ""
+
+    cidades.cidades.forEach(function(city){
+        const citiesList = document.createElement('li')
+        citiesList.textContent = city
+
+        ul.append(citiesList)
+    })
+}
+
+const getEstados = (event) => {
+    const estado = event.target.id.replace('BR-','')
+    preencherDados(estado)
 }
 
 mapa.addEventListener('click', getEstados)
